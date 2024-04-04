31fefb13978245a7e2650356ceeb9d7ad981a2ef
Quinten Bruce <qcbruce7@gmail.com>
2024-04-04 13:59:51 -0500

    rewrote movement code and added rigid body

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,49 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool isMoving;
-    private Vector2 input;
-    public float moveSpeed = 1f;
-    public LayerMask solidObjectsLayer;
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
 
-    // Update is called once per frame
+    // Called once per frame
     private void Update()
     {
-        if (!isMoving)
-        {
-            float horizontalInput = Input.GetAxisRaw("Horizontal");
-            float verticalInput = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-            if (horizontalInput != 0 || verticalInput != 0)
-            {
-                horizontalInput = verticalInput == 0 ? horizontalInput : 0;
-                Vector3 inputDirection = new Vector3(horizontalInput, verticalInput, 0f).normalized;
-                Vector3 targetPosition = transform.position + inputDirection;
-
-                if (isWalkable(targetPosition))
-                {
-                    StartCoroutine(Move(targetPosition));
-                }
-            }
-        }
     }
-
-    private bool isWalkable(Vector3 targetPosition)
+    // Called on a fixed schedule regardless of fps. Good for physics
+    private void FixedUpdate()
     {
-        if (Physics2D.OverlapCircle(targetPosition, 0.2f, solidObjectsLayer) != null)
-        {
-            return false;
-        }
-        return true;
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
-    private IEnumerator Move(Vector3 targetPos)
-    {
-        isMoving = true;
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-            yield return null;
-        }
-        isMoving = false;
-    }
+
+
 }
