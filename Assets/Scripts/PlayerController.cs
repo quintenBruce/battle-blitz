d26ed0c3cc39e35422d4ba4750bcebe663f97536
Quinten Bruce <qcbruce7@gmail.com>
2024-04-04 18:24:53 -0500

    finish animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,28 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 movement;
+    public Animator animator;
 
     // Called once per frame
     private void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        // Get the input for horizontal and vertical movement
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
+        // Normalize the movement vector to ensure consistent speed in all directions
+        movement = new Vector2(horizontalInput, verticalInput).normalized;
+
+        // Update the animator parameters
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.magnitude);
     }
+
     // Called on a fixed schedule regardless of fps. Good for physics
     private void FixedUpdate()
     {
+        // Move the Rigidbody using the normalized movement vector
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 
