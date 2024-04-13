98cd88991c18bfb4dbb0956be95d268a1976f6ee
Quinten Bruce <qcbruce7@gmail.com>
2024-04-13 16:25:25 -0500

    implemented bullet synchronization

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    Vector3 mousePos;
+    public Vector3 mousePos;
     private Camera mainCam;
     private Rigidbody2D rb;
     public float force;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        //mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         rb = GetComponent<Rigidbody2D>();
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        //mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
 
-        Vector3 direction = mousePos - transform.position;
         Vector3 rotation = transform.position - mousePos;
-        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
         float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, rot + 180);
     }
@@ -26,5 +27,14 @@ public class BulletScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void upD()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 direction = mousePos - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
     }
 }
--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class Shooting : MonoBehaviour
+public class Shooting : NetworkBehaviour
 {
+    private NetworkVariable<Vector3> m_pointer = new NetworkVariable<Vector3>(default,
+        NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     private Camera mainCam;
     private Vector3 mousePos;
     public GameObject bullet;
@@ -12,38 +16,78 @@ public class Shooting : MonoBehaviour
     public bool canFire;
     private float timer;
     public float timeBetweenFiring;
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            m_pointer.Value = bulletTransform.position;
+        }
+        else
+        {
+            m_pointer.OnValueChanged += OnPointerChanged;
+        }
+    }
+    private void OnPointerChanged(Vector3 previous, Vector3 current)
+    {
+        bulletTransform.position = current;
+    }
     void Start()
     {
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
     }
+
+    void FixedUpdate()
+    {
+        if (IsOwner)
+        {
+            m_pointer.Value = bulletTransform.position;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        
-        Vector3 rotation = mousePos - transform.position;
-
-        float rotz = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-
-        if (!canFire )
+        if (IsOwner)
         {
-            timer += Time.deltaTime;
-            if (timer > timeBetweenFiring )
+
+            mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector3 rotation = mousePos - transform.position;
+
+            float rotz = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, rotz);
+
+            if (!canFire)
             {
-                canFire = true;
-                timer = 0;
+                timer += Time.deltaTime;
+                if (timer > timeBetweenFiring)
+                {
+                    canFire = true;
+                    timer = 0;
+                }
+            }
+            if (Input.GetMouseButton(0) && canFire)
+            {
+                canFire = false;
+                mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+                mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+                SpawnBulletServerRPC(bulletTransform.position, Quaternion.identity, mousePos);
+
+
             }
         }
-        transform.rotation = Quaternion.Euler(0, 0, rotz);
-        if (Input.GetMouseButton(0) && canFire)
-        {
-            canFire = false;
-            var instance = Instantiate(bullet, bulletTransform.position, Quaternion.identity);
-            var instanceNetworkObject = instance.GetComponent<NetworkObject>();
-            instanceNetworkObject.Spawn();
+    }
 
-
-        }
+    [ServerRpc]
+    private void SpawnBulletServerRPC(Vector3 position, Quaternion rotation, Vector3 mousePos)
+    {
+        var instance = Instantiate(bullet, position, rotation);
+        BulletScript script = instance.GetComponent<BulletScript>();
+        script.mousePos = mousePos;
+        script.upD();
+        var instanceNetworkObject = instance.GetComponent<NetworkObject>();
+        instanceNetworkObject.Spawn();
     }
 }
